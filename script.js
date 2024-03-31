968926c06e0c8c40827a7570a8c0432691ea7caf
ismail <ismailbary2@gmail.com>
2024-03-31 17:54:28 +0800

    feta: add embed to youtube

--- script.js
+++ script.js
@@ -11,53 +11,94 @@ const searchURL = BASE_URL + '/search/movie?'+API_KEY;
     const response = await fetch(API_URL)
     const results = await response.json()
     return results
-  }     
+  }   
+  let currentPage = 1;
+let nextPage = 2;
+let prevPage = 3;
+// let lastUrl = '';
+let totalPages = 100;
 
-    function renderMovie (movie) {     
+  
+  getMovies(API_URL);
+
+function getMovies(url) {
+  // lastUrl = url;
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results)
+        if(data.results.length !== 0){
+            renderMovie(data.results);
+            // currentPage = data.page;
+            // nextPage = currentPage + 1;
+            // prevPage = currentPage - 1;
+            // totalPages = data.total_pages;
+
+            // current.innerText = currentPage;
+
+            // if(currentPage <= 1){
+            //   prev.classList.add('disabled');
+            //   next.classList.remove('disabled')
+            // }else if(currentPage>= totalPages){
+            //   prev.classList.remove('disabled');
+            //   next.classList.add('disabled')
+            // }else{
+            //   prev.classList.remove('disabled');
+            //   next.classList.remove('disabled')
+            // }
+
+        }else{
+            main.innerHTML= `<h1 class="no-results">No Results Found</h1>`
+        }
+       
+    })
+
+}
+
+    function renderMovie (data) {     
         
         const containerElement = document.getElementsByTagName('div')[1];
-        const data = movie.results
+        // const data = movie.results
         
-        data.forEach(element => {
-            console.log('ini adalah element yang ditampilkan' ,element)
+        data.forEach(movie => {
+            console.log('ini adalah element yang ditampilkan' ,movie)
+            const {title, poster_path, vote_average, release_date, overview, id} = movie;
 
             const divElement = document.createElement('div')
             divElement.classList.add('card-i-do')
             divElement.innerHTML = `
-            <img class="image-card" src="https://image.tmdb.org/t/p/w500${element.poster_path}" >
+            <img class="image-card" src="https://image.tmdb.org/t/p/w500${poster_path}" >
             <h3> 
-                ${element.original_title}   
+                ${title}   
             </h3>
             <div class="release-date">
-              <p>${element.release_date}</p>
+              <p>${release_date}</p>
             </div>
             <div class="overview">
 
                 <h3>Overview</h3>
                 <p>
-                ${element.overview}
+                ${overview}
                 </p>
-                <button class="know-more" id="${element.id}">Know More</button
+                <button class="know-more" id="${id}">Know More</button
             </div>
             `
             containerElement.appendChild(divElement)
 
-            document.getElementById(element.id).addEventListener('click', () => {
-              // console.log(element.id)
+            document.getElementById(id).addEventListener('click', () => {
+              console.log("ididididididid", id)
               openNav(movie)
             })
 
         });
-        
+       
         
     }
-    getMovie()
-    .then((movie) => renderMovie(movie))
+    // getMovie()
+    // .then((movie) => renderMovie(movie))
 
     const overlayContent = document.getElementById('overlay-content');
-    /* Open when someone clicks on the span element */
+    /* membuka halaman youtube ketika menglik tombol know more */
     function openNav(movie) {
-      let id = movie.results;
+      let id = movie.id;
 
       console.log("ini movie nav",id )
       fetch(BASE_URL + '/movie/'+id+'/videos?'+API_KEY)
@@ -68,8 +109,7 @@ const searchURL = BASE_URL + '/search/movie?'+API_KEY;
           document.getElementById("myNav").style.width = "100%";
           if(videoData.results.length > 0){
             let embed = [];
-            let dots = [];
-            videoData.results.forEach((video, idx) => {
+            videoData.results.forEach((video) => {
               let {name, key, site} = video
     
               if(site == 'YouTube'){
@@ -78,22 +118,14 @@ const searchURL = BASE_URL + '/search/movie?'+API_KEY;
                   <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" title="${name}" class="embed hide" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
               
               `)
-    
-                dots.push(`
-                  <span class="dot">${idx + 1}</span>
-                `)
               }
             })
             
-            var content = `
+            let content = `
             <h1 class="no-results">${movie.original_title}</h1>
             <br/>
             
             ${embed.join('')}
-            <br/>
-    
-            <div class="dots">${dots.join('')}</div>
-            
             `
             overlayContent.innerHTML = content;
             activeSlide=0;
@@ -111,13 +143,12 @@ const searchURL = BASE_URL + '/search/movie?'+API_KEY;
       document.getElementById("myNav").style.width = "0%";
     }
     
-    var activeSlide = 0;
-    var totalVideos = 0;
+    let activeSlide = 0;
+    let totalVideos = 0;
     
     function showVideos(){
       let embedClasses = document.querySelectorAll('.embed');
-      let dots = document.querySelectorAll('.dot');
-    
+          
       totalVideos = embedClasses.length; 
       embedClasses.forEach((embedTag, idx) => {
         if(activeSlide == idx){
@@ -130,34 +161,6 @@ const searchURL = BASE_URL + '/search/movie?'+API_KEY;
         }
       })
     
-      dots.forEach((dot, indx) => {
-        if(activeSlide == indx){
-          dot.classList.add('active');
-        }else{
-          dot.classList.remove('active')
-        }
-      })
     }
     
-    const leftArrow = document.getElementById('left-arrow')
-    const rightArrow = document.getElementById('right-arrow')
-    
-    leftArrow.addEventListener('click', () => {
-      if(activeSlide > 0){
-        activeSlide--;
-      }else{
-        activeSlide = totalVideos -1;
-      }
-    
-      showVideos()
-    })
-    
-    rightArrow.addEventListener('click', () => {
-      if(activeSlide < (totalVideos -1)){
-        activeSlide++;
-      }else{
-        activeSlide = 0;
-      }
-      showVideos()
-    })
     
