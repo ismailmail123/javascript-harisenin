e573eab6caa6bdbc55d92ea10e2bd9c12154ec4f
ismail <ismailbary2@gmail.com>
2024-03-31 18:04:37 +0800

    feat: modified embed

--- script.js
+++ script.js
@@ -12,39 +12,14 @@ const searchURL = BASE_URL + '/search/movie?'+API_KEY;
     const results = await response.json()
     return results
   }   
-  let currentPage = 1;
-let nextPage = 2;
-let prevPage = 3;
-// let lastUrl = '';
-let totalPages = 100;
-
   
   getMovies(API_URL);
 
 function getMovies(url) {
-  // lastUrl = url;
     fetch(url).then(res => res.json()).then(data => {
         console.log(data.results)
         if(data.results.length !== 0){
             renderMovie(data.results);
-            // currentPage = data.page;
-            // nextPage = currentPage + 1;
-            // prevPage = currentPage - 1;
-            // totalPages = data.total_pages;
-
-            // current.innerText = currentPage;
-
-            // if(currentPage <= 1){
-            //   prev.classList.add('disabled');
-            //   next.classList.remove('disabled')
-            // }else if(currentPage>= totalPages){
-            //   prev.classList.remove('disabled');
-            //   next.classList.add('disabled')
-            // }else{
-            //   prev.classList.remove('disabled');
-            //   next.classList.remove('disabled')
-            // }
-
         }else{
             main.innerHTML= `<h1 class="no-results">No Results Found</h1>`
         }
